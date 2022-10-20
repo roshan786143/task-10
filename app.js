51f8d9d91fb822d5d1ff67f9a648aacd3856a2b7
roshan786143 <roshanbabu.kng@gmail.com>
2022-10-20 15:25:02 +0530

    updated the local storage by taking an object

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
 let form = document.getElementById("addForm");
 form.addEventListener("submit", storeData);
-function storeData(e) {
-  e.preventDefault();
+function storeData(event) {
+  event.preventDefault();
   //storing input data on variables
 
-  let name1 = document.getElementById("name");
-  console.log(name1.value);
-  let email = document.getElementById("email");
-  console.log(email.value);
-  let mobile = document.getElementById("mobile");
-  console.log(mobile.value);
-  let time = document.getElementById("time");
-  console.log(time.value);
-  let times = document.getElementById("times");
-  console.log(times.value);
+  console.log("------------");
 
-  console.log("------------------------------------------------------");
+  let name = event.target.name.value;
+  console.log(name);
+
+  let email = event.target.email.value;
+  console.log(email);
+
+  let mobile = event.target.mobile.value;
+  console.log(mobile);
+
+  let date = event.target.date.value;
+  console.log(date);
+
+  let time = event.target.time.value;
+  console.log(time);
+
+  console.log("******************************");
+
+  // console.log(name1.value);
+
+  // console.log("------------------------------------------------------");
 
   // console.log(e.target.name1.value);
 
   // storing input data on local storage
 
-  localStorage.setItem("name", name1.value);
-  console.log(localStorage.getItem("name"));
-  localStorage.setItem("email", email.value);
-  console.log(localStorage.getItem("email"));
-  localStorage.setItem("mobile", mobile.value);
-  console.log(localStorage.getItem("mobile"));
-  localStorage.setItem("date", time.value);
-  console.log(localStorage.getItem("time"));
-  localStorage.setItem("time", times.value);
-  console.log(localStorage.getItem("time"));
+  let user_details = {
+    name : name,
+    email : email,
+    mobile : mobile,
+    date : date,
+    times : time
+  };
+
+  // user_details.name = name1;
+  // user_details.email = this.email;
+  // user_details.mobile = this.mobile;
+  // user_details.time = this.date;
+  // user_details.times = this.times;
+
+  let user_json_data = JSON.stringify(user_details);
+
+  localStorage.setItem("user details",user_json_data);
+
+  console.log(localStorage.getItem("user details"));
+
+
+
+  // localStorage.setItem("name", name1.value);
+  // console.log(localStorage.getItem("name"));
+  // localStorage.setItem("email", email.value);
+  // console.log(localStorage.getItem("email"));
+  // localStorage.setItem("mobile", mobile.value);
+  // console.log(localStorage.getItem("mobile"));
+  // localStorage.setItem("date", time.value);
+  // console.log(localStorage.getItem("time"));
+  // localStorage.setItem("time", times.value);
+  // console.log(localStorage.getItem("time"));
 }
